f2c0995f696a93c60971ba96aca49062c5342d5e
trimbakeshwar <61821119+trimbakeshwar@users.noreply.github.com>
2020-03-07 10:16:25 +0530

    Add files via upload

---
+++ replaceString.java
@@ -0,0 +1,16 @@
+package bridgelabz;
+
+import java.util.Scanner;
+
+public class replaceString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+//		Scanner sc = new Scanner(System.in);
+//		System.out.println("enter name");
+		String str=sc.nextLine();
+		System.out.println("hello "+str+", how are you");
+	}
+
+}
